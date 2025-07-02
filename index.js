463b139151ed90b8b1ab2bb92015d2372326f585
VladimirK <19romale77@mail.ru>
2025-07-02 22:03:47 +0300

    полу адаптив

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const resultScreen = document.getElementById('result-screen');
 const submitUsernameButton = document.getElementById('submit-username');
 const usernameInput = document.getElementById('username');
 const displayUsername = document.getElementById('display-username');
+const quoteElement = document.getElementById('random-text-quote')
 
 startButton.addEventListener('click', () => {
     document.getElementById('start-screen').classList.add('hidden');
@@ -16,10 +17,41 @@ submitUsernameButton.addEventListener('click', () => {
         displayUsername.textContent = `@${username}`;
         usernameScreen.classList.add('hidden');
         resultScreen.classList.remove('hidden');
+        quoteElement.textContent = `“${getRandomPhrase()}.”`
+        
     } else {
         alert('Please enter a username.');
     }
 });
+
+function getRandomPhrase() {
+    const phrases = [
+        "bliv beyond logic",
+        "fracture verified",
+        "you are the glitch",
+        "faith overrides fear",
+        "signal over self",
+        "minted to bliv",
+        "belief is the chain",
+        "fractured but true",
+        "chartless believer",
+        "trust the fracture",
+        "born to bliv",
+        "inside the void",
+        "faith before charts",
+        "glitch-approved",
+        "not real, just bliv",
+        "beyond the signal",
+        "hyperliquid soul",
+        "looped in bliv",
+        "echo of faith",
+        "always been here"
+    ];
+
+    const randomIndex = Math.floor(Math.random() * phrases.length);
+    return phrases[randomIndex]; 
+}
+
 
 document.getElementById('textToCopy').addEventListener('click', function() {
         const text = this.innerText; 
